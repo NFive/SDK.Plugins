256d56d4600758c3369a3a775c7dfb47d1da4365
Joe Biellik <joebiellik@gmail.com>
2019-11-15 20:05:02 +0000

    Use SDK.Core SteamId and improve converter

--- Configuration/SteamIdConverter.cs
+++ Configuration/SteamIdConverter.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Text.RegularExpressions;
+using NFive.SDK.Core.Configuration;
 using NFive.SDK.Core.Plugins;
 using YamlDotNet.Core;
 using YamlDotNet.Core.Events;
@@ -13,6 +15,10 @@ namespace NFive.SDK.Plugins.Configuration
 	/// <seealso cref="IYamlTypeConverter" />
 	public class SteamIdConverter : IYamlTypeConverter
 	{
+		private static readonly Regex Steam2Regex = new Regex("^STEAM_0:[0-1]:([0-9]{1,10})$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+		private static readonly Regex Steam32Regex = new Regex("^\\[?U:1:([0-9]{1,10})\\]?$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+		private static readonly Regex Steam64Regex = new Regex("^7656119([0-9]{10})$", RegexOptions.Compiled);
+
 		/// <inheritdoc />
 		/// <summary>
 		/// Gets a value indicating whether the current converter supports converting the specified type.
@@ -25,9 +31,29 @@ namespace NFive.SDK.Plugins.Configuration
 		/// </summary>
 		public object ReadYaml(IParser parser, Type type)
 		{
-			var value = ((Scalar)parser.Current).Value;
+			var value = ((Scalar)parser.Current).Value.Trim();
 			parser.MoveNext();
-			return new SteamId(value).ToSteam64();
+
+			long id;
+
+			if (Steam64Regex.IsMatch(value))
+			{
+				id = long.Parse(value);
+			}
+			else if (Steam2Regex.IsMatch(value))
+			{
+				id = SteamId.FromSteamId2(value);
+			}
+			else if (Steam32Regex.IsMatch(value))
+			{
+				id = SteamId.FromSteamId32(value);
+			}
+			else
+			{
+				throw new YamlException("YML input not in valid SteamID 2, 32 or 64 format");
+			}
+
+			return new SteamId(id);
 		}
 
 		/// <inheritdoc />
@@ -36,7 +62,7 @@ namespace NFive.SDK.Plugins.Configuration
 		/// </summary>
 		public void WriteYaml(IEmitter emitter, object value, Type type)
 		{
-			emitter.Emit(new Scalar(new SteamId(value.ToString()).ToSteam64().ToString()));
+			emitter.Emit(new Scalar(value.ToString()));
 		}
 	}
 }
